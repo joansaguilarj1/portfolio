ae5103024c856292ff509952c4faf2a4ef3e8641
Sebastian Aguilar <joansaguilarj@gmail.com>
2026-09-22 16:55:02 -0500

    feat: se implementa seccion proyectos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Experience from "@/components/Experience";
+import Projects from "@/components/Projects";
 // import Test from "@/components/Test";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero />
       <About />
       <Experience />
+      <Projects />
       {/* <Test /> */}
     </main>
   );
